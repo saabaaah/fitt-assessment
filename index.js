0937d09870793c5bd252c964b01fd71e12741119
Hamou Maamar Sabah <saabaaah@gmail.com>
2021-11-03 15:43:24 +0400

    adding stripe payment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const API_KEY = "sk_test_51J9Vl5EC8QUyIKkd1m0MbchhkEpBD2gCn4ZOdKL4jB83kHu6WcUpXOvQvnmIyNCqtUS7ulSW8gSa1GwUPWy7bKdX005mhMGqDf";
+const stripe = require("stripe")(API_KEY);
 
 const MAIN_DOMAIN = "http://localhost:8080";
 
@@ -10,6 +12,32 @@ app.use(express.json());
 
 app.get('/',function(req,res) {
     res.sendFile(path.join(__dirname+'/views/index.html'));
+  });
+
+// routes
+app.post("/charge", (req, res) => {
+    try {
+      stripe.customers
+        .create({
+          name: req.body.name,
+          email: req.body.email,
+          source: req.body.stripeToken
+        })
+        .then(customer =>
+          stripe.charges.create({
+            amount: req.body.amount * 100,
+            currency: "usd",
+            customer: customer.id
+          })
+        )
+        .then(() => res.render("/views/success.html"))
+        .catch(err => { 
+            console.log(err)
+            res.render("/views/success.html")
+        });
+    } catch (err) {
+      res.send(err);
+    }
   });
 
 // listening...
